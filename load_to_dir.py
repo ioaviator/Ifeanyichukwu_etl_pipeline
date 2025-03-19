8f35145ad6e1a57af2ebdd1b97d50b4bf109bca4
ioaviator <kingsolomonifeanyi@gmail.com>
2025-03-19 13:04:57 +0100

    fix: modify json response from api

--- load_to_dir.py
+++ load_to_dir.py
@@ -2,8 +2,9 @@ import json
 
 
 def load_files_to_dir(data, list_id):
+  result = data.get("result", [])
   with open(f'./data/imdb_{list_id}.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=2)
+    json.dump(result, f, ensure_ascii=False, indent=2)
     print(f'imdb_movie_list_{list_id}.json successfully loaded to folder')
 
   return True

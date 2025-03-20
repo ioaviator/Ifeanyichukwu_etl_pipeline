909640bebec660dddc669db7416ce9e809e1e98b
ioaviator <kingsolomonifeanyi@gmail.com>
2025-03-20 13:56:42 +0100

    transform json files and load as parquet file format

--- transform.py
+++ transform.py
@@ -19,15 +19,33 @@ def load_json_with_polars(file_path):
 
 def transform():
   # Use list comprehension to load each file into a DataFrame
-  df_list = [load_json_with_polars(file) for file in json_files]
+  movies_list_df = [load_json_with_polars(file) for file in json_files]
 
-  # print(df_list)
   # Concatenate all JSON DataFrames into one
-  combined_json = pl.concat(df_list)
-
+  combined_json = pl.concat(movies_list_df)
+  # Rename columns to lowercase
   combined_json = combined_json.rename({col: col.lower() for col in combined_json.columns})
 
+  #Drop unwanted columns
+  combined_json = combined_json.drop(['poster_link','no_of_votes'])
+  
   # Verify changes
-  print(combined_json.columns)
+  # print(combined_json.columns)
+
+  # Drop duplicate values
+  combined_json = combined_json.unique()
+
+  # Handle missing values in columns
+  combined_json = combined_json.with_columns([
+    pl.col("meta_score").fill_null(0),
+    pl.col('certificate').fill_null('Unspecified'),
+    pl.col('gross').fill_null(0)
+])
+  # tt = combined_json.to_pandas()
+  
+  # Load dataframe to parquet file
+  combined_json.write_parquet(f'{data_folder}/processed_data.parquet')
+  
+
 
 transform()
